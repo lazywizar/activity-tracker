579f6c83045e25c23a6bbde91ae00b15e575bb94
lazywizar <bit.varun@gmail.com>
2025-01-18 11:08:36 -0800

    Adding minute indicator for today

---
+++ client/src/components/MinutesInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const MinutesInput = ({ value, onChange, isFirstColumn = false, isToday = false }) => {
+  return (
+    <div className="relative w-full">
+      <input
+        type="number"
+        value={value}
+        onChange={onChange}
+        className={`minute-input w-full text-right ${isToday ? 'pr-4' : 'pr-1'}`}
+        placeholder=""
+        min="0"
+        max="999"
+        aria-label="Duration in minutes"
+      />
+      {isFirstColumn && (
+        <div className="absolute -top-4 left-0 text-xs text-gray-500 font-medium">
+          mins
+        </div>
+      )}
+      {isToday && (
+        <div className="absolute right-0.5 top-1/2 -translate-y-1/2 text-[9px] text-gray-400 pointer-events-none">
+          min
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MinutesInput;
